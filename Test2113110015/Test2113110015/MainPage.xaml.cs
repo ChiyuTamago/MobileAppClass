4f0db7b1d3641139a326e48c2253f5f859edb8ee
ChitawornR <>
2023-11-13 14:22:35 +0700

    ClassDay1

---
+++ Test2113110015/Test2113110015/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test2113110015
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void okButton_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World");
+            await okButton.RotateTo(360, 100);
+            await okButton.RotateTo(0, 1);
+
+        }
+    }
+}
